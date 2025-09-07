418804a668af41376ba6cc17be61e254641893cc
Gartonot <gartonot62@gmail.com>
2025-09-07 16:25:50 +0300

    feat: Добавил форму для поисков поездок

--- src/pages/HomePage.vue
+++ src/pages/HomePage.vue
@@ -1,6 +1,20 @@
 <template>
     <div>
-        home
+        <div :class="$style.form">
+            <AppInputText
+                v-model:value="tripNameOrId"
+                :class="$style.inputTextSearch"
+                iconName="magnifier"
+                placeholder="Введите название поездки или её номер"
+            />
+            <AppButton
+                text="Найти"
+                size="large"
+                :class="$style.buttonSearch"
+                @click="searchTrip()"
+            />
+        </div>
+
         <AppButton
             text="Создать поездку"
             type="text"
@@ -13,17 +27,35 @@
 <script setup lang="ts" >
 import useDrawerCreateTrip from '@/composables/useDrawerCreateTrip.ts';
 import AppButton from '@/components/ui/AppButton.vue';
+import AppInputText from '@/components/ui/AppInputText.vue';
+import { ref } from 'vue';
 
 const drawer = useDrawerCreateTrip();
 
+const tripNameOrId = ref('');
+
+const searchTrip = () => {
+    // TODO: Фильтровать список поездок по клику
+    console.log(tripNameOrId.value);
+};
 const createTrip = () => {
     drawer.openDrawer();
 };
 </script>
 
 <style module>
+.form {
+    display: flex;
+    gap: 20px;
+}
+.inputTextSearch {
+    padding-inline: 20px;
+}
+.buttonSearch {
+    max-width: 130px;
+}
 .buttonCreateTrip {
-  color: var(--color-primary);
-  border-bottom: 1px dashed var(--color-primary);
+    color: var(--color-primary);
+    border-bottom: 1px dashed var(--color-primary);
 }
 </style>
